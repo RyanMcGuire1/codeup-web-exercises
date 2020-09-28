e7e6c1db2880008410dfb3f9062f58df636cbb65
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2020-09-28 09:34:34 -0500

    created warmup files

---
+++ JS/warmup.js
@@ -0,0 +1,13 @@
+"use strict";
+console.log ("hello Rod");
+console.log ("Hello Elvira");
+console.log ("Hello Chris");
+console.log ("Hello Matt");
+console.log ("Hello Jordy");
+
+function sayHello(name){
+    console.log("Hello" + name);
+}
+
+sayHello(name:"Rod");
+sayHello(nmae:"Elvira")
