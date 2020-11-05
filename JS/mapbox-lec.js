b8e3eecf1f4241fcc78feee492eaddf1fa7a3ff0
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2020-11-05 14:55:02 -0600

    maps

--- JS/mapbox-lec.js
+++ JS/mapbox-lec.js
@@ -11,7 +11,14 @@
 // Zoom levels range from 0 up to 24, with 0 being a global view and 24 being the most detailed at street level (the max zoom level depends on the location).
 
 //TODO TOGETHER: Set map to san antonio area using the coordinates [-98.4916, 29.4252]
-
+console.log(mapboxToken);
+mapboxgl.accessToken = mapboxToken;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/satellite-streets-v11\n', // stylesheet location
+    center: [-98.4916, 29.4252], // starting position [lng, lat]
+    zoom: 9// starting zoom
+});
 //TODO: Experiment with different map styles, zoom levels, and centers. You will need to reference the mapbox docs. (~10 minutes)
 
 
@@ -25,9 +32,23 @@
 
 // TODO TOGETHER: Add a marker to the map using the following coordinates [-98.4916, 29.4260]. This marker will mark the Alamo on our map.
 // TODO TOGETHER: Change the color of the marker
+ var markerOptions = {
+        color: '#ff0000',
+        draggable: true
+}
+
+ var alamoMarker = new mapboxgl.Marker(markerOptions)
+    .setLngLat([-98.4916, 29.4260])
+    .addTo(map);
+
 
 
 // TODO: Experiment with the color, and setting the LngLat
+
+
+
+
+
 // TODO: Update the marker object to make the marker draggable. *Hint: reference the docs!
 
 
@@ -39,10 +60,23 @@
 
 
 // TODO TOGETHER: Add a popup to the map over codeup. Set the html as a paragraph that says "Codeup Rocks!"
-// TODO TOGETHER: Comment out the popup we just added. Add a popup to the alamo marker.
+var popup = new mapboxgl.Popup()
+    .setLngLat([-98.489615, 29.426827])
+    .setHTML("<h6>Codeup Rocks!</h6>")
+    .addTo(map);
 
+
+
+// TODO TOGETHER: Comment out the popup we just added. Add a popup to the alamo marker.
+var  alamoPopup = new mapboxgl.Popup()
+    .setHTML("<p>Remember the alamo</p>")
+    .addTo(map);
+
+alamoMarker.setPopup(alamoPopup)
 
 // TODO: Review the popup docs. What are some additional options we can pass to the popup?
+
+
 // TODO: Try setting the text by using ".setText()" instead of ".setHTML()"
 
 
@@ -55,15 +89,41 @@
 
 // TODO TOGETHER: Using the Geocoder helper function, log the coordinates of Codeup and recenter the map to focus on Codeup.
 //https://docs.mapbox.com/mapbox-gl-js/api/map/#map#setcenter
-
+geocode("600 Navarro St #350, San Antonio, TX 78205", mapboxToken).then(function(result) {
+    console.log(result);
+    map.setCenter(result);
+    map.setZoom(20);
+});
 
 //TODO: Using the geocode method above, add a marker at Codeup to the map
+
+geocode("600 Navarro St #350, San Antonio, TX 78205", mapboxToken).then(function(result) {
+    console.log(result);
+    map.setCenter(result);
+    map.setZoom(10);
+
+    new mapboxgl.Marker()
+        .setLngLat(result)
+        .addTo(map);
+});
+
+
+
+
 //TODO: Instead of setCenter try using map.jumpTo()
 //TODO: Instead of setCenter try using map.flyTo()
 
 
 
 // TODO TOGETHER: Reverse Geocoding: Using the reverse geocoding method, enter the coordinates {lng: -98.4861, lat: 29.4260} to get a physical address for the alamo
+
+reverseGeocode({lng: -98.4861, lat: 29.4260}, mapboxToken).then(function (result) {
+    console.log(result);
+})
+
+
+
+
 // TODO: Reverse geocode coordinates of your choice using the reverse geocode method
 
 
---
+++ JS/mapbox_maps_api.js
@@ -0,0 +1,63 @@
+console.log(mapboxToken);
+mapboxgl.accessToken = mapboxToken;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/satellite-streets-v11\n', // stylesheet location
+    center: [-98.4916, 29.4252], // starting position [lng, lat]
+    zoom: 9// starting zoom
+});
+// function geocode(search, token) {
+//     var baseUrl = 'https://api.mapbox.com';
+//     var endPoint = '/geocoding/v5/mapbox.places/';
+//     return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+//         .then(function(res) {
+//             return res.json();
+//             // to get all the data from the request, comment out the following three lines...
+//         }).then(function(data) {
+//             return data.features[0].center;
+//         });
+// }
+var jimCord = [-98.709698,29.434356]
+// geocode("302 W loop 1604, San Antonio, TX 78245", mapboxToken).then(function(result) {
+//     console.log("Jims is " +result);
+//     map.setCenter(result);
+//     map.setZoom(10);
+     var jimpop = (new mapboxgl.Popup()
+        .setLngLat(jimCord)
+        .setHTML("<h6>Jims</h6>"))
+    var jimMark =new mapboxgl.Marker()
+        .setLngLat(jimCord)
+        .setPopup(jimpop)
+        .addTo(map);
+
+
+// geocode("10622 Culebra Rd., San Antonio, TX 78251", mapboxToken).then(function(result) {
+//     console.log("IHOP cord is" + result  );
+//     map.setCenter(result);
+//     map.setZoom(10);
+// });
+    var ihopCord = [-98.700738,29.49199]
+    var ihopPop = (new mapboxgl.Popup()
+        .setLngLat(ihopCord)
+        .setHTML("<h6>IHOP</h6>"))
+    var ihopMark = new mapboxgl.Marker()
+        .setLngLat(ihopCord)
+        .setPopup(ihopPop)
+        .addTo(map);
+
+
+
+
+// geocode("8303 State Highway 151, San Antonio,TX 78245", mapboxToken).then(function(result) {
+//     console.log(result);
+//
+// });
+    var pandaExpressCord =[-98.650736, 29.436804]
+     var pandaPop = new mapboxgl.Popup()
+        .setLngLat(pandaExpressCord)
+        .setHTML("<h6>Panda Express</h6>")
+
+    var pandaMark = =new mapboxgl.Marker()
+        .setLngLat(pandaExpressCord)
+        .setPopup(pandaPop)
+        .addTo(map);
