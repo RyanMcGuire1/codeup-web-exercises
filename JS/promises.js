17e6b0ca1f4cf90fb79e9801957ced374a8eaee2
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2020-11-23 17:13:30 -0600

    Handle an API primise exercise completed

--- JS/promises.js
+++ JS/promises.js
@@ -8,7 +8,11 @@ function wait(second){
     })
 }
 
-wait(1000).then(() => console.log("You'll see this after 1 second"));
-wait(3000).then(() => console.log("You'll see this after 3 seconds"));
+// wait(1000).then(() => console.log("You'll see this after 1 second"));
+// wait(3000).then(() => console.log("You'll see this after 3 seconds"));
 
+
+fetch('https://api.github.com/users/RyanMcGuire1/events/public', {headers: {'Authorization':  githubToken}})
+    .then(response => response.json())
+    .then(response=>{console.log(response[0].created_at)})
 
